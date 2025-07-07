bed72f20efd613e3cad48e7d359c943ea5254624
Johny Jimenez <xjohnyx@icloud.com>
2025-07-07 08:07:01 +0200

    fix respuesta files

--- talleres/FINAL/pregunta-1/respuesta.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# 
-kubectl label nodes cka-worker2 env=prod
--- talleres/FINAL/pregunta-2/respuesta.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-|-
-  ETCDCTL_API=3 etcdctl --endpoints=$ENDPOINTS --cacert=$CACERT --cert=$CERT --key=$KEY
-  snapshot save /tmp/etcd-backup.db
